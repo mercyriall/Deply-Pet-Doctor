0da62b7b19bdd1e9efaaa94c5fc74536f6ed7940
mercyriall <73515384+mercyriall@users.noreply.github.com>
2024-11-21 19:30:44 +0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Mercyriall-Pet-doctor-1.0";
+const contentToCache = [
+    "Build/Pet-doctor.loader.js",
+    "Build/Pet-doctor.framework.js.unityweb",
+    "Build/Pet-doctor.data.unityweb",
+    "Build/Pet-doctor.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
